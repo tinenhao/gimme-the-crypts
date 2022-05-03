47f2422021a8696874e0491533c146421700e2f5
tinenhao <tinenhao@u.nus.edu>
2022-05-03 11:02:25 +0800

    changed font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Erbaum from './assets/fonts/Erbaum-Bold.woff'
 import {
   createTheme,
   CssBaseline,
   Theme,
   ThemeProvider,
 } from '@material-ui/core'
+
+type fontDisplay = 'swap' | 'auto'
+
+const erbaum = {
+  fontFamily: 'Erbaum',
+  fontStyle: 'semi-bold',
+  fontDisplay: 'swap' as fontDisplay,
+  fontWeight: 300,
+  src: `
+  local('Erbaum'),
+  local('Erbaum-Bold'),
+  url(${Erbaum}) format('woff')`,
+}
 
 const theme: Theme = createTheme({
   palette: {
@@ -17,11 +31,25 @@ const theme: Theme = createTheme({
       secondary: '#303244',
     },
   },
+  typography: {
+    fontFamily: "'Erbaum', sans-serif !important",
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [erbaum],
+      },
+    },
+  },
 })
+
+// Drystick Semi Bold
+// Erbaum Book
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <div>test</div>
       <CssBaseline />
     </ThemeProvider>
   )
